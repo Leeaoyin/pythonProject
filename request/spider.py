aee36c6de02edba0315b988dc4097a9700bd5e28
leeaoyin <lee_ay@163.com>
2022-05-07 11:13:54 +0800

    commit: fix code

--- request/spider.py
+++ request/spider.py
@@ -11,30 +11,28 @@ from lxml import etree
 import requests
 
 
-def doSpider():
+def do_spider():
     for page in range(1, 11):
         resp = requests.get(
             url=f'https://movie.douban.com/top250?start={(page - 1) * 25}',
             headers={'User-Agent': 'BaiduSpider'}
         )
         tree = etree.HTML(resp.text)
-        # 通过XPath语法从页面中提取电影标题
 
         for x in range(1, 26):
             title_spans = tree.xpath(f'//*[@id="content"]/div/div[1]/ol/li[{x}]/div/div[2]/div[1]/a/span[1]')
-            # 通过XPath语法从页面中提取电影评分
             rank_spans = tree.xpath(f'//*[@id="content"]/div/div[1]/ol/li[{x}]/div/div[2]/div[2]/div/span[2]')
             for title_span, rank_span in zip(title_spans, rank_spans):
                 res = f'insert into movie_rank values(null,"{title_span.text}", {rank_span.text});'
-                insert_into_table(title_span.text,res)
+                insert_into_table(title_span.text, res)
 
         # time.sleep(2)
 
 
-
-def insert_into_table(name,sql):
+def insert_into_table(name, sql):
     import pymysql
-    conn = pymysql.connect(host='your_host', port=3306, user='root', password='your password', database='db', charset='utf8mb4')
+    conn = pymysql.connect(host='your_host', port=3306, user='root', password='your password', database='db',
+                           charset='utf8mb4')
     try:
         with conn.cursor() as cursor:
             affected_rows = cursor.execute(sql)
@@ -43,13 +41,13 @@ def insert_into_table(name,sql):
         conn.commit()
     except pymysql.MySQLError as error:
         conn.rollback()
-        print(type(error),error)
+        print(type(error), error)
     finally:
         conn.close()
 
 
 def main():
-    doSpider()
+    do_spider()
 
 
 if __name__ == '__main__':
